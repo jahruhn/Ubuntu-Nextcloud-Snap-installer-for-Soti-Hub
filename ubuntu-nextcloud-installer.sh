5d01f393b871ec338dbedaba6816df5f36cdcfc3
jahruhn <48630231+jahruhn@users.noreply.github.com>
2024-11-27 14:18:01 +0100

    Update ubuntu-nextcloud-installer.sh

--- ubuntu-nextcloud-installer.sh
+++ ubuntu-nextcloud-installer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #This script updates the machine, installs upgrades, cleans up and afterwards installs the snapstore package for Nextcloud in Ubuntu.
-sudo apt update && sudo apt upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y && sudo snap install nextcloud -y
+sudo apt update && sudo apt upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y && sudo snap install nextcloud 
