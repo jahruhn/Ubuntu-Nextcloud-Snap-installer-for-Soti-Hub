e4b7c13a9c683fae104a8da7ae14817ca79d4815
jahruhn <48630231+jahruhn@users.noreply.github.com>
2024-11-28 10:58:52 +0100

    added recommended OS

--- ubuntu-nextcloud-installer.sh
+++ ubuntu-nextcloud-installer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+#Recommended OS: Latest version of Ubuntu LTS
 #This script updates the machine, installs upgrades, cleans up and afterwards installs the snapstore package for Nextcloud in Ubuntu.
 sudo apt update && sudo apt upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y && sudo snap install nextcloud 
